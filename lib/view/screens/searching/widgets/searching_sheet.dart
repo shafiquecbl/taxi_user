9e4b88ffda0f84e35e2c0f9e4c72757c0690a91b
Muhammad Shafique <shafiquecbl@gmail.com>
2023-08-13 14:42:31 +0500

    code updated

--- lib/view/screens/searching/widgets/searching_sheet.dart
+++ lib/view/screens/searching/widgets/searching_sheet.dart
@@ -11,8 +11,15 @@ class SearchingDriverSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<RideController>(builder: (con) {
-      return Padding(
+      return Container(
         padding: pagePadding,
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -60,7 +67,10 @@ class SearchingDriverSheet extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20.0),
-            CustomButton(text: 'Cancel Booking', onPressed: () {})
+            CustomButton(
+              text: 'Cancel Booking',
+              onPressed: () {},
+            )
           ],
         ),
       );
